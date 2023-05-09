a1a4952a9bc87447bebc3b676d343ed855d1ec11
Nadja Jung <nadja.jung@stud.h-da.de>
2023-05-09 12:11:05 +0200

    halloechen

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3003;
 
-let wtfCounter = 0;
-const wtfTimestamps = [];
+let halloechenCounter = 0;
+const halloechenTimestamps = [];
 
 app.use(express.json());
 
-app.post('/wtf', (req, res) => {
+app.post("/halloechen", (req, res) => {
   const now = Date.now();
-  wtfCounter++;
-  wtfTimestamps.push(now);
-  res.status(200).send({ message: 'WTF received' });
+  halloechenCounter++;
+  halloechenTimestamps.push(now);
+  res.status(200).send({ message: "HALLOECHEN received" });
 });
 
-app.get('/wtf-per-minute', (req, res) => {
+app.get("/halloechen-per-minute", (req, res) => {
   const oneMinuteAgo = Date.now() - 60 * 1000;
-  
+
   // Remove outdated timestamps
-  while (wtfTimestamps.length > 0 && wtfTimestamps[0] < oneMinuteAgo) {
-    wtfTimestamps.shift();
+  while (
+    halloechenTimestamps.length > 0 &&
+    halloechenTimestamps[0] < oneMinuteAgo
+  ) {
+    halloechenTimestamps.shift();
   }
 
-  res.status(200).send({ wtfPerMinute: wtfTimestamps.length });
+  res.status(200).send({ halloechenPerMinute: halloechenTimestamps.length });
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
