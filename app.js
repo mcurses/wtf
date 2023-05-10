9fe65f75425826fd78a0ebfac09e4c025acda82d
Jona Strickler <jona.strickler@web.de>
2023-05-10 15:59:16 +0200

    feat: splendid option and percantages/ratio

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3003;
 
 let wtfCounter = 0;
 const wtfTimestamps = [];
 
+let splendidCounter = 0;
+const splendidTimestamps = [];
+
 app.use(express.json());
 
-app.post('/wtf', (req, res) => {
+app.post("/wtf", (req, res) => {
   const now = Date.now();
   wtfCounter++;
   wtfTimestamps.push(now);
-  res.status(200).send({ message: 'WTF received' });
+  res.status(200).send({ message: "WTF received" });
+});
+app.post("/splendid", (req, res) => {
+  const now = Date.now();
+  splendidCounter++;
+  splendidTimestamps.push(now);
+  res.status(200).send({ message: "Splendid received" });
 });
 
-app.get('/wtf-per-minute', (req, res) => {
+app.get("/wtf-per-minute", (req, res) => {
   const oneMinuteAgo = Date.now() - 60 * 1000;
-  
+
   // Remove outdated timestamps
   while (wtfTimestamps.length > 0 && wtfTimestamps[0] < oneMinuteAgo) {
     wtfTimestamps.shift();
@@ -24,8 +33,33 @@ app.get('/wtf-per-minute', (req, res) => {
 
   res.status(200).send({ wtfPerMinute: wtfTimestamps.length });
 });
+app.get("/splendid-per-minute", (req, res) => {
+  const oneMinuteAgo = Date.now() - 60 * 1000;
+
+  // Remove outdated timestamps
+  while (
+    splendidTimestamps.length > 0 &&
+    splendidTimestamps[0] < oneMinuteAgo
+  ) {
+    splendidTimestamps.shift();
+  }
+
+  res.status(200).send({ splendidPerMinute: splendidTimestamps.length });
+});
+
+app.get("/ratio", (req, res) => {
+  const totalSubmits = splendidCounter + wtfCounter;
+  const splendidPercentage =
+    Math.round((splendidCounter / totalSubmits) * 100) || 0;
+  const wtfPercentage = Math.round((wtfCounter / totalSubmits) * 100) || 0;
+  const ratio = Math.round((wtfCounter / splendidCounter) * 100) / 100 || 0;
+  res.status(200).send({
+    wtfPercentage: `${wtfPercentage}%`,
+    splendidPercentage: `${splendidPercentage}%`,
+    wtfSplendidRatio: ratio,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
