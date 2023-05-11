215fdc76e8a7ed69b40cdc51a619db58467de031
Nicole Pehr <116352509+Nicolepehr@users.noreply.github.com>
2023-05-11 10:37:45 +0200

    changed counter into miau
    changed counter into miau

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3003;
 
-let wtfCounter = 0;
-const wtfTimestamps = [];
+let miauCounter = 0;
+const miauTimestamps = [];
 
 app.use(express.json());
 
-app.post('/wtf', (req, res) => {
-  const now = Date.now();
-  wtfCounter++;
-  wtfTimestamps.push(now);
-  res.status(200).send({ message: 'WTF received' });
+app.post("/miau", (req, res) => {
+    const now = Date.now();
+    miauCounter++;
+    miauTimestamps.push(now);
+    res.status(200).send({message: "maius received"});
 });
 
-app.get('/wtf-per-minute', (req, res) => {
-  const oneMinuteAgo = Date.now() - 60 * 1000;
-  
-  // Remove outdated timestamps
-  while (wtfTimestamps.length > 0 && wtfTimestamps[0] < oneMinuteAgo) {
-    wtfTimestamps.shift();
-  }
+app.get("/miau-per-minute", (req, res) => {
+    const oneMinuteAgo = Date.now() - 60 * 1000;
 
-  res.status(200).send({ wtfPerMinute: wtfTimestamps.length });
+    // Remove outdated timestamps
+    while (miauTimestamps.length > 0 && miauTimestamps[0] < oneMinuteAgo) {
+        miauTimestamps.shift();
+    }
+
+    res.status(200).send({miauPerMinute: miauTimestamps.length});
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
-
